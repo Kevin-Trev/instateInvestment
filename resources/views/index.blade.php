c9e44e24580c4af87e77a198990ef88edea1671a
val-vr <lauravalentinavillela@gmail.com>
2024-07-01 11:13:23 -0600

    Juan
    t

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Instate Investment</title>
+    <title>Instate Investmen</title>
     <link rel="icon" href="{{asset('Imagenes/android-chrome-192x192.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
