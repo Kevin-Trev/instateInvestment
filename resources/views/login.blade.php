982806c2cc5b7a815e52d864f19f0a73d6e75e5d
val-vr <lauravalentinavillela@gmail.com>
2024-07-01 20:14:34 -0600

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Iniciar sesión</title>
+    <title>Registrarse</title>
     <link rel="icon" href="{{asset('Imagenes/android-chrome-192x192.png')}}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
