594598ff6ec0c06ecf9bee51506939724500ab6a
Gerardo Cruz <160986558+xCruzDev@users.noreply.github.com>
2024-07-24 19:51:20 -0600

    Controlador Login/Register

--- app/Http/Controllers/usuariosController.php
+++ app/Http/Controllers/usuariosController.php
@@ -4,14 +4,63 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Exception;
+
 
 class UsuariosController extends Controller
 {
-    public function nuevoUsuario(Request $request){
-        $usuario = user::create([
-            'email' => $request->input('email')
-        ]);
+
+    public function nuevoUsuario(Request $request) {
+
+        try{     
+            
+            DB::beginTransaction();
+            
+            $user = new User();
+
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->Nombre = $request->Nombre;
+            $user->Apellido = $request->Apellido;
+            $user->Telefono = $request->Telefono;
+            $user->Fecha_Nacimiento = $request->Fecha_Nacimiento;
+            $user->Calificacion = 0;
+            $user->email_verified_at = now();
+            $user->remember_token = Str::random(10);
+           
+            if($user->save()){
+                DB::commit();
+                Auth::login($user);
+                return redirect('/');
+            }
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            return redirect('/')->with('error','Registro incorrecto');
+        }
+
+    }
+
+    public function login () {
+        $credenciales = request()->only('email','password');
+
+        if(Auth::attempt($credenciales)){
+            request()->session()->regenerate();
+            return redirect('/');
+        }
+        else{
+            return redirect('/')->with('error_login','E-mail o contraseña Incorrecta');
+        }
+    }
+
+    public function logout () {
+
+        Auth::logout();
+        return redirect('/');
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'Nombre',
+        'Apellido',
+        'Telefono',
+        'Fecha_Nacimiento',
+        'Calificacion'
     ];
 
     /**
