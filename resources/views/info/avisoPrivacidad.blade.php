413af1d83c126699e4647109ddc1be871e3ea77f
Brandon Ivan Monreal Garcia <brendonmonreal@gmail.com>
2024-08-12 09:43:18 -0600

    UPDATE
    Privacidad y nosotros

--- resources/views/info/avisoPrivacidad.blade.php
+++ resources/views/info/avisoPrivacidad.blade.php
@@ -42,6 +42,10 @@
             background-color: transparent;
             transition: .3s;
         }
+
+        p{
+            font-size: 20px;
+        }
     </style>
 @endsection
 
@@ -55,18 +59,17 @@
         </div>
         <div class="button-container">
             @guest
-                <a href="{{route('registro')}}"><button class="bt-white">Registrate</button></a>
-                <a href="{{route('login')}}"><button class="bt-blue" type="button">Iniciar Sesión</button></a>
+            <a href="{{route('registro')}}"><button class="bt-white">Registrate</button></a>
+            <a href="{{route('login')}}"><button class="bt-blue" type="button">Iniciar Sesión</button></a>
             @endguest
             @auth
-
             <div class="dropdown">
                 <button class="bt-blue dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 {{Auth::user()->Nombre}}
                 </button>
                 <ul class="dropdown-menu">
                     <div style="margin-top:100px"></div>
-                    <li><a class="dropdown-item" href="{{route('perfil')}}">Mi perfil</a></li>
+                    <li><a class="dropdown-item" href="{{route('perfil', Auth::user()->id)}}">Mi perfil</a></li>
                     <li><a class="dropdown-item" href="{{route('catalogo')}}">Catálogo</a></li>
                     <li>
                         <form style="display: inline" action="/logout" method="POST">
@@ -88,51 +91,69 @@
         </p>
         
         <p class="text-break lh-1">
-            <strong>1. Aceptación de Términos:</strong> Al acceder y utilizar el portal <strong>Instate Inmuebles</strong>, usted acepta estar sujeto a estos términos y condiciones. 
+            Al acceder y utilizar el portal <strong>Instate Inmuebles</strong>, usted acepta estar sujeto a estos términos y condiciones. 
             Si no está de acuerdo con alguna parte, debe abstenerse de utilizar el sitio.
         </p>
         
         <p class="text-break lh-1">
-            <strong>2. Privacidad y Protección de Datos:</strong> La información personal que proporcione al utilizar <strong>Instate Inmuebles</strong> 
-            está protegida conforme a la Ley de Protección de Datos Personales. INSTATE INVESTMENTS maneja su información de acuerdo con nuestra 
-            Política de Privacidad, que se detalla en una sección específica del portal.
+            La información personal que proporcione al utilizar <strong>Instate Inmuebles</strong> 
+            está protegida conforme a la Ley de Protección de Datos Personales.
         </p>
-        
+
+        <h1>Privacidad</h1>
+
         <p class="text-break lh-1">
-            <strong>3. Modificación de Términos:</strong> <strong>INSTATE INVESTMENTS S.A. de C.V.</strong> se reserva el derecho de modificar estos términos y condiciones 
-            en cualquier momento. Cualquier cambio entrará en vigor inmediatamente después de su publicación en el sitio. Al continuar utilizando 
-            <strong>Instate Inmuebles</strong> tras las modificaciones, usted acepta estar sujeto a los términos actualizados.
+            La información sobre usted está sujeta a nuestra Política de Privacidad.
+            Al navegar el portal, el <strong>USUARIO</strong> y/o el <strong>visitante</strong> brinda información personal, 
+            prestando su consentimiento libre, expreso e informado para que la misma sea recogida
+            y almacenada directamente en nuestra base de datos, encontrándose protegida
+            electrónicamente, utilizando los mecanismos de seguridad informática de protección
+            de la información más completos y eficaces para mantenerla en total confidencialidad,
+            conforme a la Ley de Proteccion de Datos Personales.
         </p>
-        
+        <h1>Consentimiento Expreso Y Tactico</h1>
         <p class="text-break lh-1">
-            <strong>4. Uso del Sitio:</strong> El portal <strong>Instate Inmuebles</strong> está destinado exclusivamente para personas mayores de 18 años. 
-            Los usuarios deben utilizar el sitio de manera responsable y legal, y evitar cualquier actividad que pueda dañar el sistema, los servicios, o a otros usuarios.
+            A través del portal web, toda persona podrá encontrar diversos servicios y contenidos 
+            relacionados con la búsqueda de bienes raíces y proyectos de inversion.
+            Toda persona que utilice el portal acepta incondicionalmente todos y cada uno de los presentes 
+            Términos y Condiciones de Uso, sus modificaciones y documentos complementarios y se obliga a 
+            cumplirlos sin objeciones. Si Ud. no está de acuerdo con alguno de los Términos de Uso, no 
+            utilice este Sitio. Se deja expresamente aclarado que los presentes Términos y Condiciones de 
+            Uso tienen carácter obligatorio y vinculante.
         </p>
-        
         <p class="text-break lh-1">
-            <strong>5. Limitación de Responsabilidad:</strong> <strong>INSTATE INVESTMENTS</strong> no se hace responsable de posibles errores en la información 
+            El Sitio se reserva el derecho, a su sola discreción, de modificar, alterar o de otra manera 
+            actualizar, estos Términos y Condiciones de Uso en cualquier momento. Las modificaciones entrarán 
+            en vigencia desde el momento que se indique; en su defecto, se entenderá que su aplicación es 
+            inmediata. Usando este Sitio después dichas modificaciones, usted acepta estar sujeto a dichas 
+            modificaciones, alteraciones o actualizaciones de los Términos y Condiciones de Uso, sin derecho
+             a efectuar reclamo alguno con relación a ello.
+            Ciertos contenidos utilizados a través del Sitio pueden estar alcanzados por leyes, usos de suelo,
+             normas específicas que reglamenten y complementen a los presentes Términos y Condiciones de Uso, 
+             razón por la cual se recomienda a los <strong>VISITANTES</strong> tomar conocimiento específico de ellas.
+        </p>
+
+        <h1>Uso de Sitio</h1>
+        <p class="text-break lh-1">
+            El portal <strong>Instate Inmuebles</strong> está destinado exclusivamente para personas mayores de 18 años. 
+            Los usuarios deben utilizar el sitio de manera responsable y legal, y evitar cualquier actividad que pueda 
+            dañar el sistema, los servicios, o a otros usuarios.<strong>INSTATE INVESTMENTS</strong> no se hace responsable de posibles errores en la información 
             proporcionada en el sitio ni del uso incorrecto de los servicios por parte de los usuarios. Además, no se garantiza el éxito de proyectos o 
             negocios basados en la información contenida en <strong>Instate Inmuebles</strong>.
         </p>
         
         <p class="text-break lh-1">
-            <strong>6. Propiedad Intelectual:</strong> Todo el contenido presente en <strong>Instate Inmuebles</strong>, incluidos textos, imágenes, logotipos, y gráficos, 
+            Todo el contenido presente en <strong>Instate Inmuebles</strong>, incluidos textos, imágenes, logotipos, y gráficos, 
             está protegido por derechos de autor y es propiedad de <strong>INSTATE INVESTMENTS S.A. de C.V.</strong>. Queda prohibido el uso no autorizado de dicho contenido.
         </p>
         
         <p class="text-break lh-1">
-            <strong>7. Información sobre Inmuebles:</strong> La información sobre los inmuebles publicada en <strong>Instate Inmuebles</strong> es proporcionada directamente 
+            La información sobre los inmuebles publicada en <strong>Instate Inmuebles</strong> es proporcionada directamente 
             por los vendedores finales. <strong>INSTATE INVESTMENTS</strong> no se responsabiliza por la veracidad o exactitud de esta información.
         </p>
-        
+
         <p class="text-break lh-1">
-            <strong>8. Disponibilidad de Servicios:</strong> Los servicios ofrecidos a través de <strong>Instate Inmuebles</strong> están disponibles únicamente para personas 
-            con capacidad legal para contratar. <strong>INSTATE INVESTMENTS</strong> no se responsabiliza por la seguridad de la información proporcionada por los usuarios 
-            en el portal o en sitios asociados.
-        </p>
-        
-        <p class="text-break lh-1">
-            <strong>9. Jurisdicción:</strong> Cualquier controversia relacionada con estos términos y condiciones se regirá por las leyes de México y será resuelta 
+            Cualquier controversia relacionada con estos términos y condiciones se regirá por las leyes de México y será resuelta 
             en los tribunales competentes de Torreón, Coahuila.
         </p>
     </div>
@@ -140,48 +161,48 @@
 
 <img src="{{ asset('/Imagenes/Nosequesea.png') }}" id="nosequesea">
 
-    <footer id="page-footer">
-        <div class="container-info">
-            <img src="{{ asset('/Imagenes/LOGO.png') }}">
-            <p>Instate es una marga registrada por <br>Instate Investments S.A de C.V. en <br>alianza con Design Construcciones.</p>
+<footer id="page-footer">
+    <div class="container-info">
+        <img src="{{ asset('/Imagenes/LOGO.png') }}">
+        <p>Instate es una marca registrada por <br>Instate Investments S.A de C.V. en <br>alianza con Design Construcciones.</p>
+    </div>
+  
+    <br>
+  
+    <div class="container-info" id="este">
+        <h2>Contacto</h2>
+        <hr>
+        <b>MINA 330 INT. 5 COL.CENTRO <br>MONTERREY, NUEVO LEÓN. MÉXICO <br>812-433-1672</b>
+            
+        <br>
+  
+          <div class="container-social">
+              <a href="https://www.facebook.com/instatemx" target="_blank">
+                  <img src="{{ asset('Imagenes/iconoFacebook.png')}}">
+              </a>
+          </div>
+          <div class="container-social">
+              <a href="">
+                  <img src="{{ asset('Imagenes/iconoTwitter.png')}}">
+              </a>
+          </div>
+          <div class="container-social">
+              <a href="https://www.instagram.com/instatemx/" target="_blank">
+                  <img src="{{ asset('Imagenes/iconoInstagram.png')}}">
+              </a>
+          </div>
+    </div>
+        <div class="container-info2">
+            <h2>Legal</h2>
+            <hr>
+            <a href="/legal">Aviso legal</a><br><br><a href="/nosotros">Politica de privacidad </a><br><br><a href="/nosotros">Politica de cookies</a>
         </div>
-    
-        <div class="juntos">
-            <div class="container-info">
-                <h2>Contacto</h2>
-                <hr>
-                <b>MINA 330 INT. 5 COL.CENTRO <br>MONTERREY, NUEVO LEÓN. MÉXICO <br>812-433-1672</b>
-                
-                <br>
-    
-                <div class="container-social">
-                    <a href="https://www.facebook.com/instatemx">
-                        <img src="{{ asset('Imagenes/iconoFacebook.png')}}">
-                    </a>
-                </div>
-                <div class="container-social">
-                    <a href="">
-                        <img src="{{ asset('Imagenes/iconoTwitter.png')}}">
-                    </a>
-                </div>
-                <div class="container-social">
-                    <a href="https://www.instagram.com/instatemx/">
-                        <img src="{{ asset('Imagenes/iconoInstagram.png')}}">
-                    </a>
-                </div>
-            </div>
-            <div class="container-info2">
-                <h2>Legal</h2>
-                <hr>
-                <a href="/Legal/PoliticasPrivacidad">Términos y Condiciones - Instate Inmuebles</a><br><br><br><br>
-            </div>
-            <div class="container-info2">
-                <h2>Sobre</h2>
-                <hr>
-                <a href="/nosotros">Nosotros</a><br><br><br><br>
-             </div>
+        <div class="container-info2">
+            <h2>Sobre</h2>
+            <hr>
+            <a href="/nosotros">Nosotros</a><br><br><a href="/nosotros">Misión</a><br><br><a href="/nosotros">Visión</a>
         </div>
-    </footer>
+</footer>
 @endsection
 @section('js')
 @endsection
--- resources/views/info/nosotros.blade.php
+++ resources/views/info/nosotros.blade.php
@@ -42,6 +42,10 @@
             background-color: transparent;
             transition: .3s;
         }
+
+        p{
+            font-size: 20px;
+        }
     </style>
 @endsection
 
@@ -55,18 +59,17 @@
         </div>
         <div class="button-container">
             @guest
-                <a href="{{route('registro')}}"><button class="bt-white">Registrate</button></a>
-                <a href="{{route('login')}}"><button class="bt-blue" type="button">Iniciar Sesión</button></a>
+            <a href="{{route('registro')}}"><button class="bt-white">Registrate</button></a>
+            <a href="{{route('login')}}"><button class="bt-blue" type="button">Iniciar Sesión</button></a>
             @endguest
             @auth
-
             <div class="dropdown">
                 <button class="bt-blue dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                 {{Auth::user()->Nombre}}
                 </button>
                 <ul class="dropdown-menu">
                     <div style="margin-top:100px"></div>
-                    <li><a class="dropdown-item" href="{{route('perfil')}}">Mi perfil</a></li>
+                    <li><a class="dropdown-item" href="{{route('perfil', Auth::user()->id)}}">Mi perfil</a></li>
                     <li><a class="dropdown-item" href="{{route('catalogo')}}">Catálogo</a></li>
                     <li>
                         <form style="display: inline" action="/logout" method="POST">
@@ -83,76 +86,92 @@
     <div class="mx-5">
         <h1>Sobre Nosotros</h1>
     <p class="text-break lh-1">
-        INSTATE INVESTMENTS S.A. de C.V. es una empresa mexicana especializada en servicios referidos a la compra y venta de inmuebles.
+        <strong>INSTATE INVESTMENTS S.A. de C.V.</strong> es una empresa mexicana especializada en servicios referidos
+        a la compra y venta de inmuebles.
         Nos hemos dedicado a ofrecer a nuestros clientes una amplia gama de servicios como: 
-        diagnóstico de suelo, de oportunidades de negocio, estudios de mercado, planificaciones de proyectos ejecutivos etc. 
-        Nuestro objetivo es facilitar el proceso de compra, venta y renta de inmuebles, brindando un servicio 
-        excepcional y soluciones innovadoras.
-    </p>
-    
-    <p class="text-break lh-1">
-        Nuestra misión es conectar a personas y empresas con el inmueble ideal, proporcionando un servicio 
-        personalizado y eficiente que cumpla con las expectativas de nuestros clientes, y contribuyendo al 
-        desarrollo del mercado inmobiliario en México.
-    </p>
-    <p class="text-break lh-1">
-        Buscamos ser líderes en el sector inmobiliario a nivel nacional, reconocidos por nuestra innovación, integridad 
-        y compromiso con la satisfacción del cliente, y por ofrecer una plataforma digital que transforme la 
-        manera en que las personas compran y venden inmuebles.
+        diagnóstico de suelo, de oportunidades de negocio, estudios de mercado, planificaciones 
+        de proyectos ejecutivos etc. 
+        Nuestro objetivocin <strong> Instate Inmobiliaria</strong> es facilitar el proceso de compra,
+        venta y renta de inmuebles, brindando un servicio excepcional y soluciones innovadoras.
     </p>
 
     <p class="text-break lh-1">
-        En INSTATE INVESTMENTS, nos comprometemos a ofrecer un servicio personalizado que se adapte a las 
-        necesidades individuales de cada cliente. Estamos aquí para asegurarnos de que cada transacción sea 
-        transparente, segura y exitosa.
+        Actualmente el portal se encuentre en desarrollo, pero nuestros desarrolladores se encuentran
+        arduamente trabajando para birndar el servicio perfecto para usted. Si sufre algin fallo durante
+        su estancia en la pagina, no dude en contactarnos, pues gracias a estos reportes podemos centrar
+        nuestros esfuerzos en que usted tenga el mejor servicio posible. 
+        
+    </p>
+
+    <h1>Mision</h1>
+    <p class="text-break lh-1">
+        Nuestra misión es conectar a personas y empresas con el inmueble ideal, proporcionando un 
+        servicio personalizado y eficiente que cumpla con las expectativas de nuestros clientes, 
+        y contribuyendo al desarrollo del mercado inmobiliario en México.Buscamos ser líderes en 
+        el sector inmobiliario a nivel nacional, reconocidos por nuestra innovación, integridad 
+        y compromiso con la satisfacción del cliente, y por ofrecer una plataforma digital que 
+        transforme la manera en que las personas compran y venden inmuebles.
+    </p>
+
+    <p class="text-break lh-1">
+        En <strong>INSTATE INVESTMENTS</strong>, nos comprometemos a ofrecer un servicio personalizado que se adapte a las 
+        necesidades individuales de cada cliente. 
+    </p>
+
+    <h1>Vision</h1>
+    <p class="text-break lh-1">
+    Ser la promotora inmobiliaria de referencia del sector residencial en Mexico, 
+    con vocación de implantación en todo el territorio nacional y con el humilde objetivo de hacer 
+    las cosas bien, basándonos en un modelo profesional, en unos sólidos principios y valores y en
+     la sistematización y mejora de procesos y producto. 
     </p>
     </div>
 </div>
 
 <img src="{{ asset('/Imagenes/Nosequesea.png') }}" id="nosequesea">
 
-    <footer id="page-footer">
-        <div class="container-info">
-            <img src="{{ asset('/Imagenes/LOGO.png') }}">
-            <p>Instate es una marga registrada por <br>Instate Investments S.A de C.V. en <br>alianza con Design Construcciones.</p>
+<footer id="page-footer">
+    <div class="container-info">
+        <img src="{{ asset('/Imagenes/LOGO.png') }}">
+        <p>Instate es una marca registrada por <br>Instate Investments S.A de C.V. en <br>alianza con Design Construcciones.</p>
+    </div>
+  
+    <br>
+  
+    <div class="container-info" id="este">
+        <h2>Contacto</h2>
+        <hr>
+        <b>MINA 330 INT. 5 COL.CENTRO <br>MONTERREY, NUEVO LEÓN. MÉXICO <br>812-433-1672</b>
+            
+        <br>
+  
+          <div class="container-social">
+              <a href="https://www.facebook.com/instatemx" target="_blank">
+                  <img src="{{ asset('Imagenes/iconoFacebook.png')}}">
+              </a>
+          </div>
+          <div class="container-social">
+              <a href="">
+                  <img src="{{ asset('Imagenes/iconoTwitter.png')}}">
+              </a>
+          </div>
+          <div class="container-social">
+              <a href="https://www.instagram.com/instatemx/" target="_blank">
+                  <img src="{{ asset('Imagenes/iconoInstagram.png')}}">
+              </a>
+          </div>
+    </div>
+        <div class="container-info2">
+            <h2>Legal</h2>
+            <hr>
+            <a href="/legal">Aviso legal</a><br><br><a href="/legal">Politica de privacidad </a><br><br><a href="/legal">Politica de cookies</a>
         </div>
-    
-        <div class="juntos">
-            <div class="container-info">
-                <h2>Contacto</h2>
-                <hr>
-                <b>MINA 330 INT. 5 COL.CENTRO <br>MONTERREY, NUEVO LEÓN. MÉXICO <br>812-433-1672</b>
-                
-                <br>
-    
-                <div class="container-social">
-                    <a href="https://www.facebook.com/instatemx">
-                        <img src="{{ asset('Imagenes/iconoFacebook.png')}}">
-                    </a>
-                </div>
-                <div class="container-social">
-                    <a href="">
-                        <img src="{{ asset('Imagenes/iconoTwitter.png')}}">
-                    </a>
-                </div>
-                <div class="container-social">
-                    <a href="https://www.instagram.com/instatemx/">
-                        <img src="{{ asset('Imagenes/iconoInstagram.png')}}">
-                    </a>
-                </div>
-            </div>
-            <div class="container-info2">
-                <h2>Legal</h2>
-                <hr>
-                <a href="/Legal/PoliticasPrivacidad">Términos y Condiciones - Instate Inmuebles</a><br><br><br><br>
-            </div>
-            <div class="container-info2">
-                <h2>Sobre</h2>
-                <hr>
-                <a href="/nosotros">Nosotros</a><br><br><br><br>
-             </div>
+        <div class="container-info2">
+            <h2>Sobre</h2>
+            <hr>
+            <a href="/nosotros">Nosotros</a><br><br><a href="/nosotros">Misión</a><br><br><a href="/nosotros">Visión</a>
         </div>
-    </footer>
+</footer>
 @endsection
 @section('js')
 @endsection
